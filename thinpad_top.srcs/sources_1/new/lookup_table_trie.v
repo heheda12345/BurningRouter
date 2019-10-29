5972831833996e4545418fceaf7dec8ca66c01fc
heheda <zhangch99@outlook.com>
2019-10-29 22:14:10 +0800

    bram read latency 1

--- thinpad_top.srcs/sources_1/new/lookup_table_trie.v
+++ thinpad_top.srcs/sources_1/new/lookup_table_trie.v
@@ -36,7 +36,7 @@ parameter ENTRY_ADDR_MAX = (1<<ENTRY_ADDR_WIDTH);
 
 //one trie node
 parameter CHILD_BEGIN = 0;
-parameter CHILD_END = ENTRY_ADDR_WIDTH << 4;
+parameter CHILD_END = (ENTRY_ADDR_WIDTH << 4)-1;
 parameter NXT_HOP_BEGIN = CHILD_END + 1;
 parameter NXT_HOP_END   = CHILD_END + 32;
 parameter NXT_PORT_BEGIN = CHILD_END + 33;
@@ -45,10 +45,10 @@ parameter LEN_BEGIN = CHILD_END + 35;
 parameter LEN_END = CHILD_END + 36;
 parameter VALID_POS = CHILD_END + 37;
 
-reg[ENTRY_ADDR_WIDTH-1: 0] write_addr, read_addr;
+reg[ENTRY_ADDR_WIDTH-1: 0] write_addr, read_addr, new_read_addr;
 reg[ENTRY_WIDTH-1: 0] entry, entry_to_write, entry_read;
 wire[ENTRY_WIDTH-1: 0] bram_entry_read;
-reg read_enable = 0, write_enable = 0;
+reg read_enable = 0, write_enable = 0, new_read_enable;
 reg[2:0] state = STATE_PAUSE, next_state = STATE_PAUSE;
 reg[31:0] lookup_addr;
 reg[1:0] lookup_port;
@@ -70,7 +70,7 @@ xpm_memory_sdpram #(
     .ADDR_WIDTH_B(ENTRY_ADDR_WIDTH),
     .MEMORY_SIZE(ENTRY_WIDTH * ENTRY_ADDR_MAX),
     .READ_DATA_WIDTH_B(ENTRY_WIDTH),
-    .READ_LATENCY_B(0),
+    .READ_LATENCY_B(1),
     .WRITE_DATA_WIDTH_A(ENTRY_WIDTH),
     .USE_MEM_INIT(1)
 ) trie_sdpram (
@@ -80,8 +80,8 @@ xpm_memory_sdpram #(
     .ena(write_enable),
     .wea(25'b1111111111111111111111111),
 
-    .addrb(read_addr),
-    .enb(read_enable),
+    .addrb(new_read_addr),
+    .enb(new_read_enable),
     .doutb(bram_entry_read)
 );
 
@@ -105,17 +105,18 @@ always @(posedge lku_clk) begin
     // if (write_enable)
     //     $display("write to %d: [%d %d] hop-%h port-%d len-%d valid-%d", write_addr, entry_to_write[9:0], entry_to_write[19:10], entry_to_write[NXT_HOP_END:NXT_HOP_BEGIN], entry_to_write[NXT_PORT_END: NXT_PORT_BEGIN],  entry_to_write[LEN_END:LEN_BEGIN], entry_to_write[VALID_POS]);
     // $display("state: %d-%d", state, next_state);
+    read_addr <= new_read_addr;
+    read_enable <= new_read_enable;
 end
 
 // state machine
 always @(posedge lku_clk) begin
     case (next_state)
         STATE_PAUSE: begin
-                // $display("state: pause");
+                //$display("state: pause modify %d query %d full %d", modify_in_ready, query_in_ready, full);
                 if (modify_in_ready && !full) begin
                     $display("[lookup] modify begin %h->%h", modify_in_addr, modify_in_nexthop);
                     dep <= 28;
-                    read_addr <= 1;
                     lookup_addr <= modify_in_addr;
                     lookup_port <= modify_in_nextport;
                     lookup_nexthop <= modify_in_nexthop;
@@ -128,20 +129,16 @@ always @(posedge lku_clk) begin
                         next_state <= STATE_INS_READ;
                     end
                     write_enable <= 0;
-                    read_enable <= 1;
                 end else if (query_in_ready) begin
                     $display("[lookup] query begin %h", query_in_addr);
                     dep <= 28;
-                    read_addr <= 1;
                     next_state <= STATE_QUE_READ;
                     lookup_addr <= query_in_addr;
                     query_out_nexthop <= 0;
                     query_out_nextport <= 0;
-                    read_enable <= 1;
                     write_enable <= 0;
                 end else begin
                     next_state <= STATE_PAUSE;
-                    read_enable <= 0;
                     write_enable <= 0;
                 end
             end
@@ -150,7 +147,6 @@ always @(posedge lku_clk) begin
                 entry_to_write <= {1'b1, 2'b00, lookup_port, lookup_nexthop, entry_read[CHILD_END:CHILD_BEGIN]};
                 write_addr <= 1;
                 write_enable <= 1;
-                read_enable <= 0;
                 next_state <= STATE_WAIT_END;
             end
         STATE_INS_READ: begin
@@ -159,16 +155,12 @@ always @(posedge lku_clk) begin
                     upd_child <= cur_mask_child;
                     upd_last <= cur_mask_child | upd_extend[len-1];
                     cur <= read_addr;
+                    // $display("modify range %d %d", cur_mask_child, cur_mask_child | upd_extend[len-1]);
                     if (entry_read[(cur_mask_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 0) begin
                         node_cnt <= node_cnt + 1;
-                        entry <= entry_read | 
-                            ((node_cnt + 1) << (cur_mask_child * ENTRY_ADDR_WIDTH));
-                        read_addr <= node_cnt + 1;
-                        read_enable <= 0;
+                        entry <= entry_read | ((node_cnt+1) << (cur_mask_child * ENTRY_ADDR_WIDTH));
                         write_enable <= 0;
                     end else begin
-                        read_addr <= entry_read[(cur_mask_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
-                        read_enable <= 1;
                         write_enable <= 0;
                         entry <= entry_read;
                     end
@@ -180,16 +172,11 @@ always @(posedge lku_clk) begin
                     dep <= dep-4;
                     // $display("check child: %d %d", cur_child, entry_read[(cur_child<<4)+15-: 16]);
                     if (entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 0) begin
-                        entry_to_write <= entry_read | 
-                            ((node_cnt+1) << (cur_child * ENTRY_ADDR_WIDTH));
+                        entry_to_write <= entry_read | ((node_cnt+1) << (cur_child * ENTRY_ADDR_WIDTH));
                         write_addr <= read_addr;
                         write_enable <= 1;
-                        read_enable <= 0;
-                        read_addr <= node_cnt + 1;
                         node_cnt <= node_cnt + 1;
                     end else begin
-                        read_addr <= entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
-                        read_enable <= 1;
                         write_enable <= 0;
                     end
                     next_state <= STATE_INS_READ;
@@ -197,6 +184,10 @@ always @(posedge lku_clk) begin
             end
         STATE_INS_SET: begin
                 // $display("state ins-set [valid %d len %d] len-cur %d", entry_read[VALID_POS], entry_read[LEN_END: LEN_BEGIN], len);
+                // $display("upd-child %d node_cnt %d me %d next %d nxtnxt %d", upd_child, node_cnt, 
+                    // entry[(upd_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH],
+                    // entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH],
+                    // entry[(upd_child+3)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH]);
                 if (entry_read[VALID_POS] == 0 || entry_read[LEN_END:LEN_BEGIN] < len-1)
                     entry_to_write <= {1'b1, (len[1:0]-2'b01), lookup_port, lookup_nexthop, entry_read[CHILD_END: CHILD_BEGIN]};
                 else
@@ -204,20 +195,14 @@ always @(posedge lku_clk) begin
                 write_enable <= 1;
                 write_addr <= read_addr;
                 if (upd_child != upd_last) begin
-                    if (entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 4'b0000) begin
+                    if (entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 0) begin
                         entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] <= node_cnt + 1;
-                        read_addr <= node_cnt + 1;
                         node_cnt <= node_cnt + 1;
-                        read_enable <= 0;
-                    end else begin
-                        read_addr <= entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
-                        read_enable <= 1;
                     end
                     upd_child <= upd_child + 1;
                     next_state <= STATE_INS_SET;
                 end else begin
                     next_state <= STATE_INS_UPD_SELF;
-                    read_enable <= 0;
                 end
             end
         STATE_INS_UPD_SELF: begin
@@ -225,13 +210,11 @@ always @(posedge lku_clk) begin
                 entry_to_write <= entry;
                 write_addr <= cur;
                 write_enable <= 1;
-                read_enable <= 0;
                 next_state <= STATE_WAIT_END;
             end
         STATE_WAIT_END: begin
                 // $display("state: wait for end");
                 next_state <= STATE_PAUSE;
-                read_enable <= 0;
                 write_enable <= 0;
             end
         STATE_QUE_READ: begin
@@ -242,13 +225,78 @@ always @(posedge lku_clk) begin
             end
             write_enable <= 0;
             if (entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] > 0) begin
-                read_addr <= entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
-                read_enable <= 1;
                 next_state <= STATE_QUE_READ;
                 dep <= dep-4;
             end else begin
                 next_state <= STATE_PAUSE;
-                read_enable <= 0;
+            end
+        end
+    endcase
+end
+
+always @(*) begin
+    case (next_state)
+        STATE_PAUSE: begin
+                // $display("state: pause");
+                if (modify_in_ready && !full) begin
+                    new_read_addr <= 1;
+                    new_read_enable <= 1;
+                end else if (query_in_ready) begin
+                    new_read_addr <= 1;
+                    new_read_enable <= 1;
+                end else begin
+                    new_read_enable <= 0;
+                end
+            end
+        STATE_INS_UPD_ROOT: begin
+                // $display("state: insert-upd-root");
+                new_read_enable <= 0;
+            end
+        STATE_INS_READ: begin
+                // $display("state: insert-read len %d", len);
+                if (len <= 4) begin
+                    if (entry_read[(cur_mask_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 0) begin
+                        new_read_addr <= node_cnt + 1;
+                        new_read_enable <= 0;
+                    end else begin
+                        new_read_addr <= entry_read[(cur_mask_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
+                        new_read_enable <= 1;
+                    end
+                end else begin
+                    if (entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 0) begin
+                        new_read_enable <= 0;
+                        new_read_addr <= node_cnt + 1;
+                    end else begin
+                        new_read_addr <= entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
+                        new_read_enable <= 1;
+                    end
+                end
+            end
+        STATE_INS_SET: begin
+                if (upd_child != upd_last) begin
+                    if (entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] == 0) begin
+                        new_read_addr <= node_cnt + 1;
+                        new_read_enable <= 0;
+                    end else if (entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] != node_cnt + 1)begin
+                        new_read_addr <= entry[(upd_child+2)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
+                        new_read_enable <= 1;
+                    end
+                end else begin
+                    new_read_enable <= 0;
+                end
+            end
+        STATE_INS_UPD_SELF: begin
+                new_read_enable <= 0;
+            end
+        STATE_WAIT_END: begin
+                new_read_enable <= 0;
+            end
+        STATE_QUE_READ: begin
+            if (entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH] > 0) begin
+                new_read_addr <= entry_read[(cur_child+1)*ENTRY_ADDR_WIDTH-1-: ENTRY_ADDR_WIDTH];
+                new_read_enable <= 1;
+            end else begin
+                new_read_enable <= 0;
             end
         end
     endcase
